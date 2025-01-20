5a99217d884476d65c09b29a1f0f99b6a8675292
Mohamed Ahmed Ghazali <MGhazali@salaambank.co.ug>
2025-01-20 16:33:52 +0300

    Add card component with layout, form, and styling for centered content

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("index.scss").pipe(sass()).pipe(dest("css"));
+  return src("index.scss")
+  .pipe(sass().on("error", sass.logError))
+  .pipe(dest("css"));
 }
 
 function watchTask() {
