f421ce49646728c0a1dc3c6838f85eb281c36b8d
Mohamed Ahmed Ghazali <MGhazali@salaambank.co.ug>
2025-01-21 15:27:20 +0300

    Update Gulp build task to output files to 'dist' directory

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const sass = require("gulp-sass")(require("sass"));
 function buildStyles() {
   return src("index.scss")
     .pipe(sass().on("error", sass.logError))
-    .pipe(dest("css"));
+    .pipe(dest("dist/css"));
 }
 
 function watchTask() {
