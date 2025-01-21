8d1505c12c8b7a803e82681f35727549b6ae7329
Mohamed Ahmed Ghazali <MGhazali@salaambank.co.ug>
2025-01-21 15:05:16 +0300

    Define 'build' task in Gulpfile for explicit build process

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,20 @@ const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
   return src("index.scss")
-  .pipe(sass().on("error", sass.logError))
-  .pipe(dest("css"));
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
   watch(["index.scss"], buildStyles);
 }
+
+// Define the build task explicitly
+function build() {
+  return buildStyles();
+}
+
+// Export tasks
+exports.build = build; // Now you can run 'gulp build'
 
 exports.default = series(buildStyles, watchTask);
